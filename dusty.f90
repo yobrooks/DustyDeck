7622f260071608ad354a472529ec36908365db22
Yasmine O. Brooks <brooks_yo@cobra.cs.mercer.edu>
2020-02-03 17:50:30 -0500

    Initial F90 file

---
+++ dusty.f90
@@ -0,0 +1,8 @@
+program dusty
+    implicit none
+        INTEGER, PARAMETER :: MAXDIM = 5 !parameter==constant, real value is 100
+        INTEGER, DIMENSION(MAXDIM) :: IA
+        REAL(KIND = DP), DIMENSION(MAXDIM) :: AV, BV, CV
+        REAL(KIND = DP), DIMENSION(MAXDIM, MAXDIM) :: OP, ID, AM, DM
+        REAL
+end program dusty
